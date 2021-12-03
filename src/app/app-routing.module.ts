db54ef5474fd37ec0109d790ad53b92bdde1e4bc
Sander Janssens <sander.janssens@outlook.be>
2021-12-03 11:08:10 +0100

    updated app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ItemsComponent} from "./items/items.component";
+import {ItemDetailComponent} from "./item-detail/item-detail.component";
 
 const routes: Routes = [
   { path: 'items', component: ItemsComponent },
   { path: '', redirectTo: '/items', pathMatch: 'full' },
+  { path: 'items/:id', component: ItemDetailComponent },
 ];
 
 @NgModule({
